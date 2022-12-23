cd4b25f2686bb0c91ad317dbf17a63553cc759c5
jmcho2010 <91246840+jmcho2010@users.noreply.github.com>
2022-12-23 14:53:34 +0900

    Update DemoController.java

--- src/main/java/com/javadeveloperzone/DemoController.java
+++ src/main/java/com/javadeveloperzone/DemoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @RequestMapping(value = "/")
     public String landing(){
-        return "Spring boot Gradle Example";
+        return "test";
     }
     
     @RequestMapping(value = "hello")
